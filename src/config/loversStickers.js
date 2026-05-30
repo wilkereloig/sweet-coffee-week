20cba1971e3d36268c3de1e1e4e22b2057db86f4
Wilker Eloi Galvão <wilkereloigalvao@gmail.com>
2026-05-30 14:11:24 -0300

    style: lovers stickers as larger background layer behind content

--- src/config/loversStickers.js
+++ src/config/loversStickers.js
@@ -2,12 +2,12 @@
 // Cada item posiciona um PNG no fundo da página (decorativo, aria-hidden).
 export const LOVERS_STICKERS = {
   sobre: [
-    { src: '/images/adesivos/sobre-1.png', style: { top: '6%',  left: '4%',  width: 'clamp(70px,9vw,130px)', '--rot': '-8deg' } },
-    { src: '/images/adesivos/sobre-2.png', style: { top: '18%', right: '5%', width: 'clamp(70px,9vw,130px)', '--rot': '7deg' } },
-    { src: '/images/adesivos/sobre-3.png', style: { top: '52%', left: '3%',  width: 'clamp(70px,9vw,130px)', '--rot': '5deg' } },
-    { src: '/images/adesivos/sobre-4.png', style: { top: '64%', right: '6%', width: 'clamp(70px,9vw,130px)', '--rot': '-6deg' } },
-    { src: '/images/adesivos/sobre-5.png', style: { bottom: '14%', left: '7%', width: 'clamp(70px,9vw,130px)', '--rot': '9deg' } },
-    { src: '/images/adesivos/sobre-6.png', style: { bottom: '8%', right: '4%', width: 'clamp(70px,9vw,130px)', '--rot': '-5deg' } },
+    { src: '/images/adesivos/sobre-1.png', style: { top: '6%',  left: '4%',  width: 'clamp(110px,13vw,200px)', '--rot': '-8deg' } },
+    { src: '/images/adesivos/sobre-2.png', style: { top: '18%', right: '5%', width: 'clamp(100px,12vw,180px)', '--rot': '7deg' } },
+    { src: '/images/adesivos/sobre-3.png', style: { top: '52%', left: '3%',  width: 'clamp(120px,14vw,210px)', '--rot': '5deg' } },
+    { src: '/images/adesivos/sobre-4.png', style: { top: '64%', right: '6%', width: 'clamp(110px,13vw,190px)', '--rot': '-6deg' } },
+    { src: '/images/adesivos/sobre-5.png', style: { bottom: '14%', left: '7%', width: 'clamp(100px,12vw,180px)', '--rot': '9deg' } },
+    { src: '/images/adesivos/sobre-6.png', style: { bottom: '8%', right: '4%', width: 'clamp(120px,14vw,210px)', '--rot': '-5deg' } },
   ],
   participantes: [], mapa: [], combos: [], premiacao: [],
 }
